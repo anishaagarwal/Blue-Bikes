31cbbee01bb23d08fc798e81f391004ccc8b9e54
Glitch (p5-quick-start) <none>
2022-11-13 19:36:12 +0000

    🕍🍟 Checkpoint ./script.js:48706768/1756

--- script.js
+++ script.js
@@ -21,10 +21,59 @@ function everyFeaturePopup(feature, layer) {
     "</h2>" +
     "<p>" +
     feature.properties["Name"] +
-    "</p>";
+    "</p>"+
+     "<p>" +feature.properties['Total docks']+" docks"+"</p>";
 
   layer.bindPopup(popupContent);
 }
+
+// style object applies to all markers
+    const geojsonMarkerOptions = {
+        radius: 5,
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    // create a new geojson layer and add the tooltip function
+    // also add the pointToLayer function, which will loop thru each feature in the data and return a circle marker
+    let bikes = L.geoJSON(null, {
+        onEachFeature: everyFeaturePopup,
+        pointToLayer: function (feature, latlng) {
+          if (feature.properties["District"] == "Cambridge") {
+            geojsonMarkerOptions.fillColor = "blue"
+          } else {
+            geojsonMarkerOptions.fillColor = "orange"
+          }
+          
+          if (feature.properties["Total docks"] > 20) {
+            geojsonMarkerOptions.radius = 15
+          } else if (feature.properties["Total docks"] <= 20 && feature.properties["Total docks"] > 12) {
+            geojsonMarkerOptions.radius = 10
+          } 
+          else {
+            geojsonMarkerOptions.radius = 5
+          }
+      
+          // circle marker takes two args: position and styling
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+    });
+
+
+    // set the filename for the geojson
+    const url = 'https://cdn.glitch.global/feedfe67-3b30-4f7e-bacb-65ee64aa7ecc/bikes.geojson?v=1667938381575';
+
+    // Get GeoJSON data using jQuery and create features.
+    // then attach the data to the variable we created above
+    $.getJSON(url, function (dataFromJson) {
+        bikes.addData(dataFromJson);
+    });
+
+    // add features to the map
+    bikes.addTo(map)
 
 // // this is a marker that is added to Boston
 // L.marker([42.3601, -71.0589]).addTo(map)
