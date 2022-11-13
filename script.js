5791482194fb6d2fe7f38cca5c62464208bb2e0d
Glitch (p5-quick-start) <none>
2022-11-13 19:24:47 +0000

    🚖🏂 Checkpoint ./script.js:48706768/6255

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-const map = L.map('map').setView([42.3601, -71.0589], 8);
+const map = L.map("map").setView([42.3601, -71.0589], 8);
 
-    // set the background map tiles and add to the map
-    const tiles= L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-})
-    
-    // add tiles to the map
-    tiles.addTo(map);
+// set the background map tiles and add to the map
+const tiles = L.tileLayer(
+  "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+  {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>',
+  }
+);
 
-// this is a marker that is added to Boston
-L.marker([42.3601, -71.0589]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+// add tiles to the map
+tiles.addTo(map);
 
-var circle = L.circle([42.3601, -71.0589], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(map);
+// Add a tooltip when feature is clicked
+function everyFeaturePopup(feature, layer) {
+  var popupContent =
+    "<h2>" +
+    feature.properties["District"] +
+    "</h2>" +
+    "<p>" +
+    feature.properties["Name"] +
+    "</p>";
+
+  layer.bindPopup(popupContent);
+}
+
+// // this is a marker that is added to Boston
+// L.marker([42.3601, -71.0589]).addTo(map)
+//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+
+// var circle = L.circle([42.3601, -71.0589], {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//     radius: 500
+// }).addTo(map);
