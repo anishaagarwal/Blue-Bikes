79114be066233d2ec730abfb512651eebbd7e329
Glitch (p5-quick-start) <none>
2022-11-23 06:02:37 +0000

    🌶🔋 Checkpoint ./data_commute.json:48706768/64883964 ./script.js:48706768/855 ./index.html:48706768/273

--- script.js
+++ script.js
@@ -200,6 +200,42 @@ var v3Spec = {
   }
 
 }
+
+var v4Spec = {
+  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+  "data": {
+    "url": "data/data_commute.json"
+  },
+  "width": 600,
+  "height": 400,
+  "transform": [
+    {"filter": "datum.Entity !== 'All natural disasters'"}
+  ],
+  "mark": {
+    "type": "circle",
+    "opacity": 0.8,
+    "stroke": "black",
+    "strokeWidth": 1
+  },
+  "encoding": {
+    "x": {
+      "field": "Year",
+      "type": "temporal",
+      "axis": {"grid": false}
+    },
+    "y": {"field": "Entity", "type": "nominal", "axis": {"title": ""}},
+    "size": {
+      "field": "Deaths",
+      "type": "quantitative",
+      "title": "Annual Global Deaths",
+      "legend": {"clipHeight": 30},
+      "scale": {"rangeMax": 5000}
+    },
+    "color": {"field": "Entity", "type": "nominal", "legend": null}
+  }
+}
+
  
 
 vegaEmbed("#vis3", v3Spec);
+vegaEmbed("#vis4", v4Spec);
